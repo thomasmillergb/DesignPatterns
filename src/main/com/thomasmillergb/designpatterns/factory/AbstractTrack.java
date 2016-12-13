c72cbcd05dfd0baaf6f6cc36ea8f6e3952acc128
milleth <thomas.miller@rbs.com>
2016-12-13 14:32:06 +0000

    add command patten

--- src/main/com/thomasmillergb/designpatterns/factory/AbstractTrack.java
+++ src/main/com/thomasmillergb/designpatterns/factory/AbstractTrack.java
@@ -4,27 +4,39 @@ package com.thomasmillergb.designpatterns.factory;
  * @author Thomas
  *         Created by Thomas on 10/12/2016.
  */
-public abstract  class AbstractTrack implements Track{
-    private String name_ = "No Track Name";
-    private String band_ = "No Band Name";
+public abstract class AbstractTrack implements Track
+{
+    private String name_    = "No Track Name";
+    private String band_    = "No Band Name";
+    private int    duration = 3;
 
-    protected AbstractTrack(String name, String band) {
+    protected AbstractTrack(String name, String band)
+    {
         name_ = name;
         band_ = band;
     }
 
     @Override
-    public String getName() {
+    public String getName()
+    {
         return name_;
     }
 
     @Override
-    public String getBand() {
+    public String getBand()
+    {
         return band_;
     }
 
     @Override
-    public String toString() {
+    public String getDuration()
+    {
+        return band_;
+    }
+
+    @Override
+    public String toString()
+    {
         return "\n name: " + name_ + " band: " + band_;
     }
 }
--- src/main/com/thomasmillergb/designpatterns/factory/Track.java
+++ src/main/com/thomasmillergb/designpatterns/factory/Track.java
@@ -9,4 +9,6 @@ public interface Track {
     String getName();
 
     String getBand();
+
+    String getDuration();
 }
